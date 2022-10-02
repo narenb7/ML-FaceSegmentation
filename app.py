1ba0813dc87c903ce86fb712bd403b56cc8fcaf2
narenb7 <hinarenb@gmail.com>
2022-10-02 18:01:36 -0400

    Commit app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 
 #We instantiate a deeplab model with the location of the pretrained models
 #https://github.com/tensorflow/models/tree/master/research/deeplab
-model_path =
+model_path = './frozen_inference_graph.pb'
 model = DeepLabModel(model_path)
 
 #We generate a new FastAPI app in the Prod environment
@@ -18,26 +18,27 @@ app = FastAPI(title='Face Segmentation Background Changer')  #, root_path="/Prod
 
 #The face-bokeh endpoint receives post requests with the image and returns the transformed image
 # Create an async POST function for "/face-bokeh/{query}" and look for tags = tags=["Face Bokeh"]
+@app.post("/face-bokeh/{query}", tags=["Face Bokeh"])
 async def bokeh(file: UploadFile = File(...), query: str = ''):
     #We await the string, then read it
-    contents =
+    contents = await file.read()
     #Let's convert it from a string to a 8bit image
-    nparr =
+    nparr = np.fromstring(contents, np.uint8)
     #Decode the image in color
-    img =
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     #We run the model to get the segmentation masks
-    mask =
+    mask = mode.get_mask(img)
     #We add the background
-    return_img =
+    return_img = model.transform(img, mask, query)
     #We encode the image before returning it
-    _, png_img =
+    _, png_img = cv2.imencode('.PNG', return_img)
     return StreamingResponse(io.BytesIO(png_img.tobytes()), media_type="image/png")
 
 
 #The root path will be used as the health check endpoint
-@app.get("/", ##Code here##)
-##Code here##
-
+@app.get("/",  tags=["Health Check"])
+async def root():
+    return {"message": "Ok"}
 #Mangum is an adapter for running ASGI applications in AWS Lambda
 #https://github.com/jordaneremieff/mangum
 handler = Mangum(app=app)
